ab69883dd2dc1d764b6cf31432c2e90ee5196fc2
Lubiv <lubivartur@gmail.com>
2019-03-23 18:55:24 +0100

    implemented a function to convert transaction to a dictionary

--- MonyerSolution/MonyerApp/Transaction.py
+++ MonyerSolution/MonyerApp/Transaction.py
@@ -6,4 +6,5 @@ class Transaction:
         self.isExpense = isExpense
         self.dateTime = dateTime
 
-
+    def as_dict(self):
+        return {"title": self.title, "amount": self.amount, "isExpense": self.isExpense, "dateTime": self.dateTime}
