46278892f1fbd7301232a1496fa3de87c00abd81
Lubiv <lubivartur@gmail.com>
2019-03-24 16:39:23 +0100

    adapted main, so it can use balance

--- MonyerSolution/MonyerApp/main.py
+++ MonyerSolution/MonyerApp/main.py
@@ -7,19 +7,21 @@ from Transaction import *
 from Balance import *
 
 class Program(QtWidgets.QMainWindow, programDesign.Ui_MainWindow):
-    def __init__(self, parent=None):
+    def __init__(self, current_balance, parent=None):
         super(Program, self).__init__(parent)
         self.setupUi(self)
+        self.current_balance = current_balance
         self.addButton.clicked.connect(self.add_transaction)
 
     def add_transaction(self):
-        dialog = Dialog(self)
+        dialog = Dialog(self.current_balance, self)
         dialog.show()
 
 class Dialog(QtWidgets.QDialog, dialogDesign.Ui_Dialog):
-    def __init__(self, parent=None):
+    def __init__(self, current_balance, parent=None):
         super(Dialog, self).__init__(parent)
         self.setupUi(self)
+        self.current_balance = current_balance
         self.buttonBox.accepted.connect(self.take_data)
         self.buttonBox.rejected.connect(self.close)
 
@@ -29,14 +31,15 @@ class Dialog(QtWidgets.QDialog, dialogDesign.Ui_Dialog):
         transaction = Transaction(self.titleEdit.text(),
                                   self.amountEdit.value(),
                                   self.expenseRadioButton.isChecked(),
-                                  self.dateTimeEdit.dateTime().toString(self.dateTimeEdit.displayFormat())
-        #currentBalance.add(transaction)
-        self.close();
+                                  self.dateTimeEdit.dateTime().toString(self.dateTimeEdit.displayFormat()))
+        self.current_balance.add(transaction)
+        self.close()
         
 
 def main():
+    current_balance = Balance(10)
     app = QtWidgets.QApplication(sys.argv)
-    mainWindow = Program()
+    mainWindow = Program(current_balance)
     mainWindow.show()
     app.exec_()
 
