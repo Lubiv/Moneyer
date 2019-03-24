34287dd6016d976dec812848e230d022a95165ad
Lubiv <lubivartur@gmail.com>
2019-03-24 16:38:49 +0100

    refactored balance class so it can be initialized loaded, saved and appended

--- MonyerSolution/MonyerApp/Balance.py
+++ MonyerSolution/MonyerApp/Balance.py
@@ -3,15 +3,14 @@ from Transaction import *
 
 class Balance:
 
-    def __init__(self, transactions, initial):
-        self.balance = pd.DataFrame({"title": "initial", "amount": initial, "isExpense": "", "dateTime": ""})
+    def __init__(self, initial):
+        self.balance = pd.DataFrame({"title": "initial", "amount": [initial], "isExpense": "", "dateTime": ""})
 
-        for i in range(transactions.count):
-            self.balance.loc[1 + i] = transactions[i].as_dict();
-
-        
     def save(self):
-        balance.to_csv('test.csv');
+        self.balance.to_csv('test.csv')
 
-    def load(self):        
-        return pd.read_csv('test.csv')
+    def load(self, path):        
+        self.balance = pd.read_csv(path)
+
+    def add(self, transaction):
+        self.balance.loc[len(self.balance.index)] = transaction.as_dict()
